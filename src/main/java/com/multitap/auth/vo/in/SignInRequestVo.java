6bacdd31851c7302c70c341c6242d1556a6a8c3b
kang-subin <114653066+kang-subin@users.noreply.github.com>
2024-10-28 23:29:47 +0900

    Feat: SignInRequestVo 수정
    swagger example 작성

--- src/main/java/com/multitap/auth/vo/in/SignInRequestVo.java
+++ src/main/java/com/multitap/auth/vo/in/SignInRequestVo.java
@@ -1,9 +1,14 @@
 package com.multitap.auth.vo.in;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class SignInRequestVo {
+
+    @Schema(description = "이메일", example = "test1234@gmail.com")
     private String email;
+
+    @Schema(description = "비밀번호", example = "!test1234")
     private String password;
 }
